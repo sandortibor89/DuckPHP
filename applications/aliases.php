2a176fd8ffaaf83291760e81ec7e602f3e3af3d7
sandortibor89 <tibikeh@gmail.com>
2017-02-01 16:17:10 +0100

    Core
    Public assets urls bug

--- applications/aliases.php
+++ applications/aliases.php
@@ -1,17 +1,19 @@
 <?php
 return [
-    '4u.dev' => '4u.plus',
-    '4you.plus' => '4u.plus',
-    'hungary.com.ru' => '4u.plus',
-    'pannon.ru' => '4u.plus',
-    'spirro.ru' => '4u.plus',
-    'trade2hungary.com' => '4u.plus',
-    'trade2russia.com' => '4u.plus',
-    'championwines.dev' => 'championwines.hu',
-    'winexport.dev' => 'winexport.hu',
-    'montetokaj.hu' => 'winexport.hu',
-    'sandortibor.dev' => 'sandortibor.hu',
-    'kissszilard.dev' => 'kissszilard.hu',
-    'wine-tour.dev' => 'wine-tour.hu',
-    'hazifeladat.dev' => 'hazifeladat.hu'
+	'4u.dev' => '4u.plus',
+	'4you.plus' => '4u.plus',
+	'hungary.com.ru' => '4u.plus',
+	'pannon.ru' => '4u.plus',
+	'spirro.ru' => '4u.plus',
+	'trade2hungary.com' => '4u.plus',
+	'trade2russia.com' => '4u.plus',
+
+	'championwines.dev' => 'championwines.hu',
+	'winexport.dev' => 'winexport.hu',
+	'montetokaj.hu' => 'winexport.hu',
+	'sandortibor.dev' => 'sandortibor.hu',
+	'kissszilard.dev' => 'kissszilard.hu',
+	'wine-tour.dev' => 'wine-tour.hu',
+	'hazifeladat.dev' => 'hazifeladat.hu',
+
 ];
