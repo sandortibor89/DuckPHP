eadbce50b072217585f40b4a76378d692cf153f2
sandortibor89 <tibikeh@gmail.com>
2017-04-26 11:24:52 +0200

    Update
    Spaces to tabs.

--- system/core/controller.php
+++ system/core/controller.php
@@ -2,34 +2,34 @@
 namespace core;
 
 if (class_exists('\model\Authentication')) {
-    class Controller extends \model\Authentication {
+	class Controller extends \model\Authentication {
 
 		protected $loaded;
 
-        public function __construct() {
-            parent::__construct();
-            $backup = Router::get()['mysqlbackup'];
-            if ($backup == 1) {
-                $filename='database_backup_'.MYSQL_DATABASE.'_'.date('Y_m_d_H_i_s').'.sql';
-                exec('mysqldump --user='.MYSQL_USERNAME.' --password='.MYSQL_PASSWORD.' --host='.MYSQL_HOST.' '.MYSQL_DATABASE.' > '.BACKUPS_DIR.DS.$filename);
-            } elseif (file_exists(APP_SUB_DIR.DS.'all_controller_load.php')) {
+		public function __construct() {
+			parent::__construct();
+			$backup = Router::get()['mysqlbackup'];
+			if ($backup == 1) {
+				$filename='database_backup_'.MYSQL_DATABASE.'_'.date('Y_m_d_H_i_s').'.sql';
+				exec('mysqldump --user='.MYSQL_USERNAME.' --password='.MYSQL_PASSWORD.' --host='.MYSQL_HOST.' '.MYSQL_DATABASE.' > '.BACKUPS_DIR.DS.$filename);
+			} elseif (file_exists(APP_SUB_DIR.DS.'all_controller_load.php')) {
 				$this -> loaded = require_once(APP_SUB_DIR.DS.'all_controller_load.php');
 			}
-        }
-    }
+		}
+	}
 } else {
-    class Controller {
+	class Controller {
 
 		protected $loaded;
 
-        public function __construct() {
-            $backup = Router::get()['mysqlbackup'];
-            if ($backup == 1) {
-                $filename='database_backup_'.MYSQL_DATABASE.'_'.date('Y_m_d_H_i_s').'.sql';
-                exec('mysqldump --user='.MYSQL_USERNAME.' --password='.MYSQL_PASSWORD.' --host='.MYSQL_HOST.' '.MYSQL_DATABASE.' > '.BACKUPS_DIR.DS.$filename);
-            } elseif (file_exists(APP_SUB_DIR.DS.'all_controller_load.php')) {
+		public function __construct() {
+			$backup = Router::get()['mysqlbackup'];
+			if ($backup == 1) {
+				$filename='database_backup_'.MYSQL_DATABASE.'_'.date('Y_m_d_H_i_s').'.sql';
+				exec('mysqldump --user='.MYSQL_USERNAME.' --password='.MYSQL_PASSWORD.' --host='.MYSQL_HOST.' '.MYSQL_DATABASE.' > '.BACKUPS_DIR.DS.$filename);
+			} elseif (file_exists(APP_SUB_DIR.DS.'all_controller_load.php')) {
 				$this -> loaded = require_once(APP_SUB_DIR.DS.'all_controller_load.php');
 			}
-        }
-    }
+		}
+	}
 }
--- system/core/model.php
+++ system/core/model.php
@@ -2,9 +2,9 @@
 namespace core;
 
 class Model {
-    
-    public static function __callStatic($name, $arguments) {
-        return Load::model($name, $arguments);
-    }
-    
+
+	public static function __callStatic($name, $arguments) {
+		return Load::model($name, $arguments);
+	}
+
 }
