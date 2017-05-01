8f297ae982258b9148e5f173062576d40bb5a77f
sandortibor89 <tibikeh@gmail.com>
2017-05-01 18:21:58 +0200

    Update
    Bug fixed

--- system/core/controller.php
+++ system/core/controller.php
@@ -17,6 +17,7 @@ class Controller extends Auth {
 		if ($backup == 1) {
 			$filename='database_backup_'.MYSQL_DATABASE.'_'.date('Y_m_d_H_i_s').'.sql';
 			exec('mysqldump --user='.MYSQL_USERNAME.' --password='.MYSQL_PASSWORD.' --host='.MYSQL_HOST.' '.MYSQL_DATABASE.' > '.BACKUPS_DIR.DS.$filename);
+			exit;
 		} elseif (file_exists(APP_SUB_DIR.DS.'all_controller_load.php')) {
 			$this -> loaded = require_once(APP_SUB_DIR.DS.'all_controller_load.php');
 		}
